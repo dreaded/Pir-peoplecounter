f2eff7501a430c449ace46a495062f60b61f1d75
dreaded <i.am.ze.nemesis@gmail.com>
2016-10-31 12:51:38 +0000

    Add files via upload

---
+++ peoplecount.py
@@ -0,0 +1,51 @@
+
+from ubidots import ApiClient
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.IN) ## pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(10, GPIO.OUT)
+try:
+
+    api = ApiClient("ec49ebc15fb54ddc35d715fef4bc24aebd3fe01f")
+
+    people = api.get_variable("57fccc9d762542079f5a557c")
+
+except:
+
+    print "Couldn't connect to the API, check your Internet connection"
+
+counter = 0
+
+peoplecount = 0
+
+while(1):
+
+    i=GPIO.input(8)
+
+    if (i==True):
+    
+        peoplecount += 1
+        
+        GPIO.output(10,1)
+
+        presence = 0
+
+        time.sleep(1.5)
+
+    time.sleep(1)
+
+    counter += 1
+
+    if(counter==5):
+
+        print peoplecount
+
+        people.save_value({'value':peoplecount})
+
+        counter = 0
+
+        peoplecount = 0
+
+    GPIO.output(10,0)
